f68add738116f0d9bf8250cafe4ae1da5825b849
Rogier den Breejen <rogier.den.breejen@gmail.com>
2023-03-01 00:49:34 +0100

    changes

--- create-table.R
+++ create-table.R
@@ -37,25 +37,29 @@ tbl <- reactable(data,
               minWidth = 180,
               name = ""
             ),
-            `Flink CDC` = colDef(
+            `Flink` = colDef(
               html=TRUE,
               align = "center",
-              cell = function(value) column_transformer(value)
+              cell = function(value) column_transformer(value),
+              name = "<img alt='' src='https://flink.apache.org/flink-header-logo.svg'<p>CDC</p>"
             ),
-            `Debezium + Kafka Connect` = colDef(
+            `Debezium` = colDef(
               html=TRUE,
               align = "center",
-              cell = function(value) column_transformer(value)
+              cell = function(value) column_transformer(value),
+              name = "<img alt='' src='https://debezium.io/assets/images/color_black_debezium_type_600px.svg'><p>+ Kafka Connect / Debezium server</p>"
             ),
             `Airbyte` = colDef(
               html=TRUE,
               align = "center",
-              cell = function(value) column_transformer(value)
+              cell = function(value) column_transformer(value),
+              name = "<img alt='' src='https://assets.website-files.com/605e01bc25f7e19a82e74788/624d9c4a375a55100be6b257_Airbyte_logo_color_dark.svg' style='width: 64.50px; height: 49.06px'>"
             ),
             `Singer` = colDef(
               html=TRUE,
               align = "center",
-              cell = function(value) column_transformer(value)
+              cell = function(value) column_transformer(value),
+              name = "<img alt='' src='https://www.singer.io/img/singer_logo_full_black.svg'>" #style="max-width: none!important; width: 238.80px; height: 169.74px; margin-left: -47.67px; margin-top: -68.85px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);" title=""
             )
           ),
           theme = reactableTheme(

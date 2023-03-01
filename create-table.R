8bcf8db1f417cb6bcbc46f07f0782f87c44f3f1a
Rogier den Breejen <rogier.den.breejen@gmail.com>
2023-03-02 00:37:23 +0100

    changes

--- create-table.R
+++ create-table.R
@@ -58,12 +58,6 @@ tbl <- reactable(data,
               align = "center",
               cell = function(value) column_transformer(value),
               name = "<img alt='' src='https://assets.website-files.com/605e01bc25f7e19a82e74788/624d9c4a375a55100be6b257_Airbyte_logo_color_dark.svg' style='width: 64.50px; height: 49.06px'>"
-            ),
-            `Singer` = colDef(
-              html=TRUE,
-              align = "center",
-              cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://www.singer.io/img/singer_logo_full_black.svg'>" #style="max-width: none!important; width: 238.80px; height: 169.74px; margin-left: -47.67px; margin-top: -68.85px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);" title=""
             )
           ),
           theme = reactableTheme(

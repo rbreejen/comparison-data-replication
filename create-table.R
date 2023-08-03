3cb77a9fefc818c025cd305c062d55c5a7c02136
Rogier den Breejen <32416114+rbreejen@users.noreply.github.com>
2023-08-04 00:15:05 +0200

    CSS changes

--- create-table.R
+++ create-table.R
@@ -11,11 +11,11 @@ data <- fread(here::here("comparison-data-replication.csv"),quote="", header=T)
 
 column_transformer <- function(value) {
   if (str_detect(value, "%N%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%N%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b33d02ef39b1fc3adfb_icon_x.png' style='width: 20.48px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+    markdown::renderMarkdown(stringr::str_replace(value, "%N%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b33d02ef39b1fc3adfb_icon_x.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
   } else if (str_detect(value, "%Y%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%Y%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b327bf0d9337241e112_check.png' style='width: 20.48px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+    markdown::renderMarkdown(stringr::str_replace(value, "%Y%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b327bf0d9337241e112_check.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
   } else if (str_detect(value, "%U%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%U%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b3276df8460a3e8d91b_output-onlinepngtools.png' style='width: 25.56px; height: 20.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+    markdown::renderMarkdown(stringr::str_replace(value, "%U%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b3276df8460a3e8d91b_output-onlinepngtools.png' style='width: 20.46px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
   } else if (!is.na(as.numeric(value))) {
     span(class = "number", value)
   } else {
@@ -55,32 +55,37 @@ tbl <- reactable(data,
               align = "center",
               cell = function(value) column_transformer(value),
               class = "border-left",
-              name = "<img alt='' src='https://debezium.io/assets/images/color_black_debezium_type_600px.svg'>"
+              minWidth = 90,
+              name = "<img alt='' src='https://debezium.io/assets/images/color_black_debezium_type_600px.svg#svgView(viewBox(0, 0, 600, 130))' />"  #"<img alt='' src='https://debezium.io/assets/images/color_black_debezium_type_600px.svg'>"
             ),            
             `Flink` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://flink.apache.org/img/logo/png/1000/flink_squirrel_1000.png' style='width: 64.50px; height: 49.06px'<p>Flink CDC</p>"
+              minWidth = 90,
+              name = "<img alt='' src='https://flink.apache.org/img/logo/png/1000/flink_squirrel_1000.png' style='width: 32.0px; height: 32.0px'><p style='padding-top: 3px;'>Flink CDC</p>"
             ),
             `Airbyte` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://assets.website-files.com/605e01bc25f7e19a82e74788/624d9c4a375a55100be6b257_Airbyte_logo_color_dark.svg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='https://assets.website-files.com/605e01bc25f7e19a82e74788/624d9c4a375a55100be6b257_Airbyte_logo_color_dark.svg#svgView(viewBox(-20, -50, 500, 200))' style='width: 83.85px; height: 33.54px'>"
             ),
             `Estuary` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://docs.estuary.dev/img/estuary-new.png' style='width: 64.50px; height: 49.06px'><p>Estuary</p>"
+              minWidth = 90,
+              name = "<img alt='' src='https://docs.estuary.dev/img/estuary-new.png' style='width: 32.0px; height: 32.0px'><p>Estuary</p>"
             ),            
             `AWS` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
+              minWidth = 90,
               class = "border-left",
-              name = "<img alt='' src='https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg' style='width: 64.50px; height: 49.06px'><p>DMS</p>"
+              name = "<img alt='' src='https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg' style='width: 48.38px; height: 36.80px'><p>DMS</p>"
             ),
             # `Azure` = colDef(
             #   html=TRUE,
@@ -92,56 +97,66 @@ tbl <- reactable(data,
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://brandeps.com/logo-download/G/Google-Cloud-logo-vector-01.svg' style='height: 49.06px'><p>GCP Datastream</p>"
+              minWidth = 90,
+              name = "<img alt='' src='https://brandeps.com/logo-download/G/Google-Cloud-logo-vector-01.svg' style='width: 48.38px; height: 36.80px'><p>GCP Datastream</p>"
             ),            
             `Qlik` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://upload.wikimedia.org/wikipedia/commons/3/32/Qlik_Logo.svg' style='width: 64.50px; height: 49.06px'><p>Replica</p>"
+              minWidth = 90,
+              name = "<img alt='' src='https://upload.wikimedia.org/wikipedia/commons/3/32/Qlik_Logo.svg' style='width: 56.09px; height: 16.43px'><p>Replica</p>"
             ),
             `Fivetran` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://assets-global.website-files.com/6130fa1501794ed4d11867ba/63d9599008ad50523f8ce26a_logo.svg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='https://assets-global.website-files.com/6130fa1501794ed4d11867ba/63d9599008ad50523f8ce26a_logo.svg' style='width: 80.63px; height: 22.40px'>"
             ),
             `Arcion` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
+              minWidth = 90,
               class = "border-left",              
-              name = "<img alt='' src='https://assets.website-files.com/60ec496582f5cfe61dfe9c82/630b6bf7da07e33466d680f2_arcion-logo.svg' style='width: 64.50px; height: 49.06px'>"
+              name = "<img alt='' src='https://assets.website-files.com/60ec496582f5cfe61dfe9c82/630b6bf7da07e33466d680f2_arcion-logo.svg' style='width: 77.40px; height: 15.56px'>"
             ),
             `Decodable` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='./logos/decodable.svg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='./logos/decodable.svg' style='width: 83.59px; height: 12.61px'>"
             ),
             `Striim` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='./logos/striim.jpg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='./logos/striim.jpg' style='width: 59.97px; height: 22.9px'>"
             ),
             `StreamSets` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://streamsets.b-cdn.net/wp-content/uploads/StreamSets-softwareAG-full-color-crop.svg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='https://streamsets.b-cdn.net/wp-content/uploads/StreamSets-softwareAG-full-color-crop.svg' style='width: 83.85px; height: 23.66px'>"
             ),
             `Upsolver` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='./logos/upsolver.svg' style='width: 64.50px; height: 49.06px'>"
+              minWidth = 90,
+              name = "<img alt='' src='./logos/upsolver.svg' style='width: 83.85px; height: 19.97px'>"
+              
             ),
             `Hevo` = colDef(
               html=TRUE,
               align = "center",
               cell = function(value) column_transformer(value),
-              name = "<img alt='' src='https://lever-client-logos.s3.us-west-2.amazonaws.com/3d73b5a9-c725-424d-962c-495d3d85c72f-1684919086376.png' style='width: 51.00px; height: 17.50px'>"
+              minWidth = 90,
+              name = "<img alt='' src='https://lever-client-logos.s3.us-west-2.amazonaws.com/3d73b5a9-c725-424d-962c-495d3d85c72f-1684919086376.png' style='width: 63.75px; height: 21.88px'>"
             )
           ),
           highlight = TRUE,
@@ -180,4 +195,4 @@ html_file <- "table.html"
 save_html(tbl, file = html_file)
 
 Sys.setenv(OPENSSL_CONF="/dev/null")
-webshot::webshot(url = html_file, file = "img.png", delay = 0.1, vwidth = 1028)
+webshot::webshot(url = html_file, file = "img.png", delay = 0.1, vwidth = 2000)

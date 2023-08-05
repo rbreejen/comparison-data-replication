2ff0c816722f49869726af48b8bccb3be4c514e9
Rogier den Breejen <32416114+rbreejen@users.noreply.github.com>
2023-08-05 20:10:39 +0200

    Change syntax to more general markdown-native syntax

--- create-table.R
+++ create-table.R
@@ -10,12 +10,12 @@ lapply(list.of.packages, require, character.only = TRUE)
 data <- fread(here::here("comparison-data-replication.csv"),quote="", header=T)
 
 column_transformer <- function(value) {
-  if (str_detect(value, "%N%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%N%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b33d02ef39b1fc3adfb_icon_x.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
-  } else if (str_detect(value, "%Y%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%Y%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b327bf0d9337241e112_check.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
-  } else if (str_detect(value, "%U%")) {
-    markdown::renderMarkdown(stringr::str_replace(value, "%U%", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b3276df8460a3e8d91b_output-onlinepngtools.png' style='width: 20.46px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+  if (str_detect(value, ":n:")) {
+    markdown::renderMarkdown(stringr::str_replace(value, ":n:", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b33d02ef39b1fc3adfb_icon_x.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+  } else if (str_detect(value, ":y:")) {
+    markdown::renderMarkdown(stringr::str_replace(value, ":y:", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b327bf0d9337241e112_check.png' style='width: 16.38px; height: 12.8px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
+  } else if (str_detect(value, ":u:")) {
+    markdown::renderMarkdown(stringr::str_replace(value, ":u:", "<span><img alt='' src='https://uploads-ssl.webflow.com/61f2440c9fcbc37831846652/62fc3b3276df8460a3e8d91b_output-onlinepngtools.png' style='width: 20.46px; height: 16.00px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);' title=''></i></span>"))
   } else if (!is.na(as.numeric(value))) {
     span(class = "number", value)
   } else {
